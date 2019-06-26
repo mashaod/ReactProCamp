59357c8d5f4ac19a8946e20784557383e0e46452
mashaod <mashaod1993@gmail.com>
2019-06-26 23:35:46 +0300

    add theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from './components/header';
 
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from "./theme";
+
 function App() {
   return (
-    <Header />
+    <MuiThemeProvider theme={theme}>
+      <Header />
+    </MuiThemeProvider>
   )
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+  primary: { main: '#8E24AA' },
+  secondary: { main: '#FFEB3B' }
+};
+const themeName = 'Seance Gorse Sheep';
+
+export default createMuiTheme({ palette, themeName });
