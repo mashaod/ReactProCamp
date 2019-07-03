90b0cc1c921e195ae0242c7aa29ba284db185aa7
mashaod <mashaod1993@gmail.com>
2019-07-03 21:19:17 +0300

    change team country to team city

--- src/components/commandList/components/TeamsByRowGroups.js
+++ src/components/commandList/components/TeamsByRowGroups.js
@@ -8,17 +8,16 @@ import TableRow from '@material-ui/core/TableRow';
 const TeamsByRowGroups = (props) => {
   const { teams, count } = props;
 
-  const teamsGroups = _.chunk(teams, count);
-
+  const teamGroups = _.chunk(teams, count);
   return (
-    teamsGroups.map((group) => (
+    teamGroups.map((group) => (
       <TableRow key={'group_' + group[0].team_id}>
 
         {group.map((team) =>
           <TableCell key={team.team_id} align="center" component="th" scope="row">
             <h2>{ team.name }</h2>
             <img src={team.logo} alt="team logo"></img>
-            <p><i>{ team.country }</i></p>
+            <p><i>{ team.venue_city }</i></p>
           </TableCell>
         )}
 
