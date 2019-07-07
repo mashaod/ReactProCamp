29e9145666b927ad7d85a574ca9c3509e3d23d13
mashaod <mashaod1993@gmail.com>
2019-07-08 00:59:50 +0300

    change structure of action creators

---
+++ src/actions/actions.js
@@ -0,0 +1,54 @@
+/*
+ * action types
+ */
+
+export const FETCH_TEAMS_REQUEST = 'FETCH_TEAMS_REQUEST'
+export const FETCH_TEAMS_SUCCESS = 'FETCH_TEAMS_SUCCESS'
+export const FETCH_TEAMS_FAILURE = 'FETCH_TEAMS_FAILURE'
+export const FETCH_TEAM_BY_ID_REQUEST = 'FETCH_TEAM_BY_ID_REQUEST'
+export const FETCH_TEAM_BY_ID_SUCCESS = 'FETCH_TEAM_BY_ID_SUCCESS'
+export const FETCH_TEAM_BY_ID_FAILURE = 'FETCH_TEAM_BY_ID_FAILURE'
+
+/*
+ * action creators
+ */
+
+export function teamsRequested() {
+    return {
+        type: 'FETCH_TEAMS_REQUEST'
+    };
+};
+
+export function teamsLoaded(teams) {
+    return {
+        type: 'FETCH_TEAMS_SUCCESS',
+        payload: teams
+    };
+};
+
+export function teamsError(error) {
+    return {
+        type: 'FETCH_TEAMS_FAILURE',
+        payload: error
+    };
+};
+
+export function teamRequested() {
+    return {
+        type: 'FETCH_TEAM_BY_ID_REQUEST'
+    };
+};
+
+export function teamLoaded(data) {
+    return {
+        type: 'FETCH_TEAM_BY_ID_SUCCESS',
+        payload: data
+    };
+};
+
+export function teamError(error) {
+    return {
+        type: 'FETCH_TEAM_BY_ID_FAILURE',
+        payload: error
+    };
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,30 @@
-import { fetchTeams } from './teamList.action';
-import { fetchTeamById } from './teamCard.action';
+import { 
+    teamsRequested,
+    teamsLoaded,
+    teamsError,
+    teamRequested,
+    teamLoaded,
+    teamError
+} from './actions';
 
+const fetchTeams = (appService) => () => (dispatch) => {
+    dispatch(teamsRequested());
+    appService.getTeams()
+        .then(({ teams }) => dispatch(teamsLoaded(teams)))
+        .catch((err) => dispatch(teamsError(err)));
+};
+
+const fetchTeamById = (appService) => (teamId) => (dispatch) => {
+    dispatch(teamRequested());
+    appService.getTeamById(teamId)
+        .then(({ teams }) => {
+            const message = teams.length > 0 ? null : 'Team does not exist';
+            const team = teams[0] || [];
+            return dispatch(teamLoaded({ team, message }));
+        })
+        .catch((err) => dispatch(teamError(err)));
+};
+  
 export {
     fetchTeams,
     fetchTeamById
