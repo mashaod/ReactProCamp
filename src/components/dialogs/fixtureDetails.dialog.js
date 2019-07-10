62edfcccef75bfc524bdf36805ada65f177a0a2f
mashaod <mashaod1993@gmail.com>
2019-07-11 00:34:24 +0300

    add the fixture details dialog

---
+++ src/components/dialogs/fixtureDetails.dialog.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+
+const DialogTitle = withStyles(styles)(props => {
+    const { children, classes, onClose } = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label="Close" className={classes.closeButton} onClick={onClose}>
+                <CloseIcon />
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+});
+
+const DialogContent = withStyles(theme => ({
+    root: {
+        padding: theme.spacing(2),
+    },
+}))(MuiDialogContent);
+
+const FixtureDetailsDialog = ({ handleClose, open, fixture }) => {
+    return (
+        <div>
+            <Dialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                    Fixture
+                </DialogTitle>
+
+                <DialogContent dividers>
+                    <Typography gutterBottom>
+                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
+                        facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
+                        at eros.
+                    </Typography>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}
+  
+export default FixtureDetailsDialog;
--- src/components/fixtureList/index.js
+++ src/components/fixtureList/index.js
@@ -9,6 +9,7 @@ import { fetchFixtures } from '../../actions';
 
 import ErrorIndicator from '../errorIndicator';
 import PreloaderCircular from '../preloaderCircular';
+import FixtureDetailsDialog from '../dialogs/fixtureDetails.dialog';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -16,8 +17,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import MoreVert from '@material-ui/icons/MoreVert';
 
 class FixtureList extends Component {
+
+    state = {
+        isOpenFixtureDialog: false
+    }
 
     componentDidMount() {
         this.props.fetchFixtures();
@@ -31,57 +37,75 @@ class FixtureList extends Component {
         }).format(new Date(time))
     }
 
+    openFixtureDetails = (fixtureId) => {
+        this.setState({ isOpenFixtureDialog: true });
+        //this.props.fetchFixture(fixtureId);
+    }
+
+    handleClose = () => {
+        this.setState({ isOpenFixtureDialog: false });
+    }
+
     render() {
         const { fixtures, loading, error } = this.props;
+        const { isOpenFixtureDialog } = this.state;
 
         if (loading) { return <PreloaderCircular />; }
         if (error) { return <ErrorIndicator />; }
 
         return (
-            <Paper>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="right">Home team</TableCell>
-                    <TableCell align="right"></TableCell>
-                    <TableCell align="center">Score</TableCell>
-                    <TableCell align="left"></TableCell>
-                    <TableCell align="left">AwayTeam</TableCell>
-                    <TableCell align="right">Date</TableCell>
-                    <TableCell align="left">Status</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {fixtures.map(({ fixture_id, homeTeam, awayTeam, goalsHomeTeam, goalsAwayTeam, event_date, status }) => (
-                    <TableRow key={fixture_id}>
-                        <TableCell align="right">
-                            <Link to={`/teams/${homeTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                {homeTeam.team_name}
-                            </Link>
-                        </TableCell>
-                        <TableCell align="right">
-                            <Link to={`/teams/${homeTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <img src={homeTeam.logo} height="45px" alt={`logo_${homeTeam.team_name}`}/>
-                            </Link>
-                        </TableCell>
-                        <TableCell align="center">{goalsHomeTeam} : {goalsAwayTeam}</TableCell>
-                        <TableCell align="left">
-                            <Link to={`/teams/${awayTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <img src={awayTeam.logo} height="45px" alt={`logo_${awayTeam.team_name}`}/>
-                            </Link>
-                        </TableCell>
-                        <TableCell align="left">
-                            <Link to={`/teams/${awayTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                {awayTeam.team_name}
-                            </Link>
-                        </TableCell>
-                        <TableCell align="right">{this.getDate(event_date)}</TableCell>
-                        <TableCell align="left">{status}</TableCell>
+            <React.Fragment>
+                <Paper>
+                <Table>
+                    <TableHead>
+                    <TableRow>
+                        <TableCell align="right">Home team</TableCell>
+                        <TableCell align="right"></TableCell>
+                        <TableCell align="center">Score</TableCell>
+                        <TableCell align="left"></TableCell>
+                        <TableCell align="left">AwayTeam</TableCell>
+                        <TableCell align="right">Date</TableCell>
+                        <TableCell align="left">Status</TableCell>
+                        <TableCell align="right"></TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Paper>
+                    </TableHead>
+                    <TableBody>
+                    {fixtures.map(({ fixture_id, homeTeam, awayTeam, goalsHomeTeam, goalsAwayTeam, event_date, status }) => (
+                        <TableRow key={fixture_id}>
+                            <TableCell align="right">
+                                <Link to={`/teams/${homeTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    {homeTeam.team_name}
+                                </Link>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Link to={`/teams/${homeTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <img src={homeTeam.logo} height="45px" alt={`logo_${homeTeam.team_name}`}/>
+                                </Link>
+                            </TableCell>
+                            <TableCell align="center">{goalsHomeTeam} : {goalsAwayTeam}</TableCell>
+                            <TableCell align="left">
+                                <Link to={`/teams/${awayTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <img src={awayTeam.logo} height="45px" alt={`logo_${awayTeam.team_name}`}/>
+                                </Link>
+                            </TableCell>
+                            <TableCell align="left">
+                                <Link to={`/teams/${awayTeam.team_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    {awayTeam.team_name}
+                                </Link>
+                            </TableCell>
+                            <TableCell align="right">{this.getDate(event_date)}</TableCell>
+                            <TableCell align="left">{status}</TableCell>
+                            <TableCell align="left">
+                                <MoreVert color="primary" style={{ cursor: 'pointer'}} onClick={() => this.openFixtureDetails(fixture_id)} />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </Paper>
+
+                <FixtureDetailsDialog open={isOpenFixtureDialog} handleClose={this.handleClose}/>
+            </React.Fragment>
           );
     }
 }
