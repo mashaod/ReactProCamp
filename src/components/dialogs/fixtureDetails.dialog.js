755e0e27fc8bc9e44861d6a7d969710a70cb8099
mashaod <mashaod1993@gmail.com>
2019-07-18 23:53:05 +0300

    add an event details table

--- src/components/dialogs/fixtureDetails.dialog.js
+++ src/components/dialogs/fixtureDetails.dialog.js
@@ -6,6 +6,13 @@ import MuiDialogContent from '@material-ui/core/DialogContent';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Icon from '../icon';
 
 const styles = theme => ({
     root: {
@@ -41,27 +48,69 @@ const DialogContent = withStyles(theme => ({
 }))(MuiDialogContent);
 
 const FixtureDetailsDialog = ({ open, handleClose, fixture, loading, error }) => {
-    return (
-        <div>
-            <Dialog
-                onClose={handleClose}
-                aria-labelledby="customized-dialog-title"
-                open={open}
-            >
-                <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-                    Fixture
-                </DialogTitle>
+    if (!open) return null;
 
-                <DialogContent dividers>
-                    <Typography gutterBottom>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
-                        facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
-                        at eros.
-                    </Typography>
-                </DialogContent>
-            </Dialog>
-        </div>
+    return (
+        <Dialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
+            fullWidth={true}
+            maxWidth={'md'}
+        >
+            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                Fixture details
+            </DialogTitle>
+
+            <DialogContent dividers>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={3}
+                >
+                    <Grid container item xs={3} direction="column" justify="center" alignItems="center">
+                        <img src={fixture.homeTeam.logo} alt="homeTeam logo"/>
+                        <h2>{fixture.homeTeam.team_name}</h2>
+                    </Grid>
+                    <Grid container item xs={3} justify="center" alignItems="center">
+                        <Typography align="center" variant="h1">{fixture.goalsHomeTeam} : {fixture.goalsAwayTeam}</Typography>
+                    </Grid>
+                    <Grid container item xs={3} direction="column" justify="center" alignItems="center">
+                        <img src={fixture.awayTeam.logo} alt="awayTeam logo"/>
+                        <h2>{fixture.awayTeam.team_name}</h2>
+                    </Grid>
+                </Grid>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell align="left">Event</TableCell>
+                            <TableCell align="right">Elapsed</TableCell>
+                            <TableCell align="right">Team</TableCell>
+                            <TableCell align="right">Player</TableCell>
+                            <TableCell align="right">Detail</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {fixture.events && fixture.events.map(event => (
+                        <TableRow key={event.elapsed + event.player_id + event.detail}>
+                            <TableCell align="right">
+                                <Icon name={event.detail} width={30} height={30}/>
+                            </TableCell>
+                            <TableCell align="left">{event.type}</TableCell>
+                            <TableCell align="right">{event.elapsed}</TableCell>
+                            <TableCell align="right">{event.teamName}</TableCell>
+                            <TableCell align="right">{event.player}</TableCell>
+                            <TableCell align="right">{event.detail}</TableCell>
+                        </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </DialogContent>
+        </Dialog>
     );
 }
-  
+
 export default FixtureDetailsDialog;
