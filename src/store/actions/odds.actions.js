ab200fa9cde1f6ca4ff618eb48d5118950b22f65
mashaod <mashaod1993@gmail.com>
2019-07-31 23:58:53 +0300

    add odds data

--- src/store/actions/odds.actions.js
+++ src/store/actions/odds.actions.js
@@ -29,8 +29,37 @@ const fetchOdds = (appService) => () => (dispatch) => {
 
     appService.getOdds()
         .then(({ odds }) => {
-            console.log(odds)
-            dispatch(oddsLoaded(odds))
+            const newOddsData = [];
+
+            odds.forEach((odd, index)=> {
+                appService.getMockFixture(odd.fixture.fixture_id)
+                        .then(({ fixtures }) => {
+                            if (!fixtures || !fixtures || !fixtures.length|| !odd.bookmakers || !odd.bookmakers[0]) {
+                                if (index === odds.length) return newOddsData;
+                                return;
+                            }
+
+                            const fixture = fixtures[0];
+                            const winnerBet = odd.bookmakers[0].bets.find(b => b.label_id === 1);
+                            const homeOdd = winnerBet.values.find(b => b.value === 'Home');
+                            const awayOdd = winnerBet.values.find(b => b.value === 'Away');
+
+                            const data = {
+                                event_date: fixture.event_date,
+                                homeTeam: fixture.homeTeam,
+                                awayTeam: fixture.awayTeam,
+                                homeOdd: homeOdd.odd,
+                                awayOdd: awayOdd.odd
+                            }
+
+                            newOddsData.push(data);
+                            if (index + 1 === odds.length) dispatch(oddsLoaded(newOddsData))
+                        })
+                        .catch(err => {
+                            if (newOddsData.length > 0) dispatch(oddsLoaded(newOddsData));
+                            dispatch(oddsError(err));
+                        })
+            });
         })
         .catch((err) => dispatch(oddsError(err)));
 };
